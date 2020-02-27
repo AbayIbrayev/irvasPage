643df9e93b87efdb53a0490450832dfd30904f45
Abay <abayibrayev7@gmail.com>
2020-02-27 07:52:47 +0000

    state of the calculator modals form elements is saved into the modalState object

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -2,19 +2,30 @@ import checkNumInputs from './checkNumInputs';
 
 const changeModalState = (state) => {
   const windowForm = document.querySelectorAll('.balcon_icons_img'),
-    windowWidth = document.querySelector('#width'),
-    windowHeight = document.querySelector('#height'),
-    windowType = document.querySelector('#view_type'),
+    windowWidth = document.querySelectorAll('#width'),
+    windowHeight = document.querySelectorAll('#height'),
+    windowType = document.querySelectorAll('#view_type'),
     windowProfile = document.querySelectorAll('.checkbox');
 
   checkNumInputs('#width');
   checkNumInputs('#height');
 
-  windowForm.forEach((item, i) => {
-    item.addEventListener('click', () => {
-      state.form = i;
+  function bindActionToElems(event, elem, prop) {
+    elem.forEach((item, i) => {
+      item.addEventListener(event, () => {
+        if (elem.length > 1) {
+          state[prop] = i;
+        } else {
+          state[prop] = item.value;
+        }
+        console.log(state);
+      });
     });
-  });
+  }
+
+  bindActionToElems('click', windowForm, 'form');
+  bindActionToElems('input', windowHeight, 'height');
+  bindActionToElems('input', windowWidth, 'width');
 };
 
 export default changeModalState;
